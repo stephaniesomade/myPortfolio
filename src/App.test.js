11d09d2c5baa104e42567d954562e0f7e7888350
stephaniesomade <stephaniesomadee@gmail.com>
2024-04-01 01:54:28 +0100

    feat-001: Adjust Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/jest-dom"; 
 import App from "./App";
 import React from "react";
 
 test("check that the app component renders on the page", () => {
   render(<App />);
-  const tester = screen.getByText("Hey");
+  const tester = screen.getByText(/Hey/i);
   expect(tester).toBeInTheDocument()
 });
